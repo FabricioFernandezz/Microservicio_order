ac61531fe8c16e2ebaec58db172e423e559e9996
FabricioFernandezz <fabriciofernandezz2001@gmail.com>
2024-03-26 18:36:44 -0300

    test db y actualizaciones

--- test/test_app.py
+++ test/test_app.py
@@ -12,16 +12,6 @@ class AppTestCase(unittest.TestCase):
     def tearDown(self):
         self.app_context.pop()
       
-    def test_order(self):
-        # Crea una instancia de la clase Order con valores adecuados
-        order = Order(id_order=1, id_client='client_id', id_product='product_id', payment_method='cash', total='100')
-
-        # Verifica los atributos de la orden
-        self.assertEqual(order.id_order, 1)
-        self.assertEqual(order.id_client, 'client_id')
-        self.assertEqual(order.id_product, 'product_id')
-        self.assertEqual(order.payment_method, 'cash')
-        self.assertEqual(order.total, '100')
 
     def test_app(self):
          self.app.config['TESTING'] = True
--- test/test_db.py
+++ test/test_db.py
@@ -1,38 +1,24 @@
-import unittest 
-from flask import current_app
-from sqlalchemy import text
-from app.models.order import Order
-
-from app import create_app, db
+import psycopg2
+import unittest
 
 
-class AppTestCase(unittest.TestCase):
-    
-    def setUp(self):
-        self.app = create_app()
-        self.app_context = self.app.app_context()
-        self.app_context.push()
-        db.create_all()
+class TestDatabase(unittest.TestCase): 
 
-    def tearDown(self):
-        db.session.remove()
-        db.drop_all()
-        self.app_context.pop()
+    def setUp(self): #creo la conexion a la base de datos
 
-    def test_db_connection(self):
-        result = db.session.query(text("hello word"))
-        self.assertEqual(result[0], "hello word")
-    
-    def test_app(self):
-        self.assertTrue(self.app.testing)
-        self.assertTrue(current_app.config['TESTING'])
+        self.conn = psycopg2.connect("postgresql://postgres:9697@localhost:5432/microservicio_order")
+        self.cur = self.conn.cursor()
         
-    def test_order(self):
-        order = Order(id_order=1, id_client='client_id', id_product='product_id', payment_method='cash', total='100')
+    def tearDown(self): #cierro la conexion a la base de datos
 
-        # Verifica los atributos de la orden
-        self.assertEqual(order.id_order, 1)
-        self.assertEqual(order.id_client, 'client_id')
-        self.assertEqual(order.id_product, 'product_id')
-        self.assertEqual(order.payment_method, 'cash')
-        self.assertEqual(order.total, '100')
+        self.cur.close()
+        self.conn.close()
+
+    def test_connection(self): #test de conexion a la base de datos
+        self.assertIsNotNone(self.conn)
+
+  
+    
+if __name__ == '__main__':
+    unittest.main()
+
