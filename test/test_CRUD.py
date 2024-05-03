be95f0071f695f7e9ceffea5154c65efc269ddd2
FabricioFernandezz <fabriciofernandezz2001@gmail.com>
2024-05-03 12:47:37 -0300

    Refactor CRUD tests

--- test/test_CRUD.py
+++ test/test_CRUD.py
@@ -22,31 +22,46 @@ class TestCRUD(unittest.TestCase):
     def test_create(self):
         order = Order(id_order=1, id_client='client_id', id_product='product_id', payment_method='cash', total='100')
         created_order = self.order_service.create(order)
-        self.assertIsNotNone(created_order)
+        self.assertEqual(created_order.id_order, 1)
+        self.assertEqual(created_order.id_client, 'client_id')
+        self.assertEqual(created_order.id_product, 'product_id')
+        self.assertEqual(created_order.payment_method, 'cash')
+        self.assertEqual(created_order.total, '100')
+
 
     def test_find_by_id(self):
         order = Order(id_order=1, id_client='client_id', id_product='product_id', payment_method='cash', total='100')
         self.order_service.create(order)
         found_order = self.order_service.find_by_id(1)
-        self.assertIsNotNone(found_order)
+        self.assertEqual(found_order.id_order, 1)
+        self.assertEqual(found_order.id_client, 'client_id')
+        self.assertEqual(found_order.id_product, 'product_id')
+        self.assertEqual(found_order.payment_method, 'cash')
+        self.assertEqual(found_order.total, '100')
+
 
     def test_find_all(self):
         order = Order(id_order=1, id_client='client_id', id_product='product_id', payment_method='cash', total='100')
         self.order_service.create(order)
         orders = self.order_service.find_all()
-        self.assertIsNotNone(orders)
+        self.assertEqual(len(orders), 1)
+        self.assertEqual(orders[0].id_order, 1)
+        self.assertEqual(orders[0].id_client, 'client_id')
+        self.assertEqual(orders[0].id_product, 'product_id')
+        self.assertEqual(orders[0].payment_method, 'cash')
+        self.assertEqual(orders[0].total, '100')
 
     def test_update(self):
         order = Order(id_order=1, id_client='client_id', id_product='product_id', payment_method='cash', total='100')
         self.order_service.create(order)
         updated_order = self.order_service.update(Order(id_order=1, id_client='client_id', id_product='product_id', payment_method='credit', total='100'), 1)
-        self.assertIsNotNone(updated_order)
-    
+        self.assertEqual(updated_order.payment_method, 'credit')
+        
     def test_delete(self):
         order = Order(id_order=1, id_client='client_id', id_product='product_id', payment_method='cash', total='100')
         created_order = self.order_service.create(order)
-        deleted_order = self.order_service.delete(created_order.id_order)
-        self.assertIsNotNone(deleted_order)
+        self.order_service.delete(created_order.id_order)
+        self.assertIsNone(self.order_service.find_by_id(1))
         
 if __name__ == '__main__':
     unittest.main()
