2e24d29e3c38598ce9e068132760a456713ad241
FabricioFernandezz <fabriciofernandezz2001@gmail.com>
2024-07-18 08:50:39 -0300

    Actualizacion funcional de test db y redis

--- test/test_db.py
+++ test/test_db.py
@@ -1,47 +1,29 @@
 import unittest
-from app import create_app, db
-from sqlalchemy import text
-from app.models import Order
+import psycopg2
+import os
+from dotenv import load_dotenv
+
 
 class DatabaseTestCase(unittest.TestCase):
 
+
     def setUp(self):
-        self.app = create_app()
-        self.app_context = self.app.app_context()
-        self.app_context.push()
-        db.create_all()
+        load_dotenv()
+        self.connection = psycopg2.connect(
+            user=os.getenv("POSTGRES_USER"),
+            password=os.getenv("POSTGRES_PASSWORD"),
+            database=os.getenv("POSTGRES_DB"),
+            port=os.getenv("PORT_DB")
+        )
+        self.cursor = self.connection.cursor()
 
     def tearDown(self):
-        db.session.remove()
-        db.drop_all()
-        self.app_context.pop()
-
-    def test_db_connection(self):
-        result= db.session.query(text("'probando'")).one()
-        self.assertEqual(result[0], 'probando')
+        self.cursor.close()
+        self.connection.close()
     
-    def test_create_order(self):
-        order = Order(id_order=1,id_client='client_id', id_product='product_id', payment_method='cash', total='100')
-        db.session.add(order)
-        db.session.commit()
-        self.assertIsNotNone(order.id_order)
+    def test_db_connection(self):
+        print("Testing database connection")
+        self.assertIsNotNone(self.connection)
 
-    def test_update_order(self):
-        order = Order(id_order=1,id_client='client_id', id_product='product_id', payment_method='cash', total='100')
-        db.session.add(order)
-        db.session.commit()
-        order.total = '200'
-        db.session.commit()
-        self.assertEqual(order.total, '200')
-
-    def test_delete_order(self):
-        order = Order(id_order=1,id_client='client_id', id_product='product_id', payment_method='cash', total='100')
-        db.session.add(order)
-        db.session.commit()
-        db.session.delete(order)
-        db.session.commit()
-        self.assertIsNone(Order.query.get(1))
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
--- test/test_redis.py
+++ test/test_redis.py
@@ -1,14 +1,41 @@
 import unittest
 import redis
+import os
+import time
+from dotenv import load_dotenv
 
 class TestRedis(unittest.TestCase):
     def setUp(self):
-        self.redis = redis.Redis(host='localhost', port=6379, db=0, password='9697')
+        load_dotenv()
+        self.redis = redis.Redis(
+            host=os.getenv("REDIS_HOST"), 
+            port=os.getenv("REDIS_PORT"), 
+            db=os.getenv("REDIS_DB"), 
+            password=os.getenv('REDIS_PASSWORD'))
+        self.redis.flushdb()
 
-    def test_redis_set_get(self):
+    def tearDown(self):
+        self.redis.flushdb()
+
+    def test_redis_connection(self):
+        print("\n--- Testing the Redis Connection ---")
+        self.assertIsNotNone(self.redis)
+
+    def test_redis_set(self):
+        print("\n--- Testing the Redis Set ---")
         self.redis.set('key', 'value')
-        value = self.redis.get('key')
-        self.assertEqual(value, b'value')
+        self.assertEqual(self.redis.get('key').decode('utf-8'), 'value')
+
+    def test_redis_expire(self):
+        print("\n--- Testing the Redis Expire ---")
+        self.redis.set('key', 'value', ex=1)
+        time.sleep(2)
+        self.assertIsNone(self.redis.get('key'))
+
+    def test_redis_delete(self):
+        print("\n--- Testing the Redis Delete ---")
+        self.redis.set('key', 'value')
+        self.redis.delete('key')
 
 if __name__ == '__main__':
     unittest.main()
